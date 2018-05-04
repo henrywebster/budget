e4aa1ce8707a6ed197fbc0ed267eedb0fedd17a3
henry webster <hwebster@mail.depaul.edu>
2018-05-03 23:32:13 -0500

    Adding basic files to get workflow
    Created a trivial unit test and package.

--- src/info/hwebster/budget/hello.kt
+++ src/info/hwebster/budget/hello.kt
@@ -1,5 +1,8 @@
 package info.hwebster.budget
 
+/**
+ * This is a proof-of concept for creating a Kotlin project.
+ */
 fun main(args : Array<String>) {
     println("Hello, budget!")
 }
---
+++ test/TestExample.kt
@@ -0,0 +1,12 @@
+import kotlin.test.assertTrue
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestExample {
+
+    @Test
+    fun test1() {
+        assertTrue(false);
+    }
+}
